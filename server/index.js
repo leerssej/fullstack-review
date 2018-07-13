b0667242a625184c6a54fa4f98acf3f519bff682
leerssej <jens.leerssen@gmail.com>
2018-07-13 11:25:39 -0700

    responds when knocked on get door

--- server/index.js
+++ server/index.js
@@ -8,11 +8,16 @@ app.post('/repos', function (req, res) {
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
+  
 });
 
-app.get('/repos', function (req, res) {
+app.get('/repos', function (req, res, next) {
   // TODO - your code here!
   // This route should send back the top 25 repos
+  console.log('ID:', req.params.id)
+  next()
+}, function (req, res, next) {
+  res.send('There is an answer on the GET door when knocked')
 });
 
 let port = 1128;
