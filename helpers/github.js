e1eb21b8fa3b350b42649ca4605df53c9f703586
leerssej <jens.leerssen@gmail.com>
2018-07-14 13:37:57 -0700

    gets data from github.API

--- helpers/github.js
+++ helpers/github.js
@@ -1,20 +1,33 @@
 const request = require('request');
 const config = require('../config.js');
 
-let getReposByUsername = (/* TODO */) => {
+let getReposByUsername = (repoName) => {
   // TODO - Use the request module to request repos for a specific
   // user from the github API
 
   // The options object has been provided to help you out, 
   // but you'll have to fill in the URL
   let options = {
-    url: 'FILL ME IN',
+    url: `https://api.github.com/users/${repoName}/repos`,
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
     }
   };
 
+  function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var info = JSON.parse(body);
+      console.log(info);
+      console.log(info.forks_count + " Forks");
+    }
+  };
+
+  request(options, callback);
 }
 
 module.exports.getReposByUsername = getReposByUsername;
+
+// curl https://api.github.com/search/repositories?q=stars:>70000&order=desc
+
+// getReposByUsername('leerssej');
