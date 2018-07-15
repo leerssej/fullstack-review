a6ed57656ed228c0ea2247ca931a39f9bc65a2f1
leerssej <jens.leerssen@gmail.com>
2018-07-15 11:52:56 -0700

    divs are displaying

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
-const RepoList = (props) => (
-  <div>
-    <h4> Repo List Component </h4>
-    There are {props.repos.length} repos.
-  </div>
-)
+const RepoList = (props) => {
+  const repos = props.repos;
+  const listItems = repos.map((repo, idx) => 
+    <li key={idx.toString()}>{repo.name}</li>
+  );
+  return (
+    <div>
+      <h4> Repo List Component </h4>
+      There are {props.repos.length} repos.
+      <ul>{listItems}</ul>
+    </div>
+  );
+}
 
 export default RepoList;
